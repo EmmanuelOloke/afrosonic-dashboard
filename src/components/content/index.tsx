2c8468aaf8022cd029506823122ddd9c2a0b0db5
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-15 13:00:23 +0100

    rendered the sidebar in the main content

---
+++ src/components/content/index.tsx
@@ -0,0 +1,19 @@
+import { VStack, HStack } from '@chakra-ui/react';
+
+import { Sidebar } from 'components/sidebar';
+
+export const Content = () => {
+  return (
+    <HStack width="full" flex={1} overflow="hidden">
+      <Sidebar />
+      <VStack px={12} pt={12} width="full" height="full" spacing={6} overflow="hidden">
+        {/* SearchBar */}
+        {/* Genres Carousel */}
+        <HStack width="full" alignItems="flex-start" overflow="hidden" flex={1}>
+          {/* Top Picks */}
+          {/* Top Producers */}
+        </HStack>
+      </VStack>
+    </HStack>
+  );
+};
