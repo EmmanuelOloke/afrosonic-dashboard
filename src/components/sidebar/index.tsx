d2e1b28a9761c25b0d0ed234bd53047aea376307
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-15 12:59:27 +0100

    render the navItems in the sidebar

---
+++ src/components/sidebar/index.tsx
@@ -0,0 +1,28 @@
+import { List, ListItem, VStack } from '@chakra-ui/react';
+
+import { NavItem } from './nav-item';
+import { navItems } from './nav-data';
+
+export const Sidebar = () => {
+  return (
+    <VStack
+      alignItems="flex-start"
+      width="full"
+      height="full"
+      maxW={{ base: 56, '2xl': 72 }}
+      borderRightColor="gray.dark"
+      borderRightWidth={2}
+      flexShrink={0}
+    >
+      {/* Logo */}
+
+      <List width="full" overflowY="auto">
+        {navItems.map((item, index) => (
+          <ListItem key={item.label}>
+            <NavItem item={item} isActive={index === 0} />
+          </ListItem>
+        ))}
+      </List>
+    </VStack>
+  );
+};
