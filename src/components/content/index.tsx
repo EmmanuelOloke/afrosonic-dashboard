71964c85b23165b94103a68e4afe40641213c154
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-15 14:00:14 +0100

    rendered the search bar component

--- src/components/content/index.tsx
+++ src/components/content/index.tsx
@@ -1,13 +1,14 @@
 import { VStack, HStack } from '@chakra-ui/react';
 
 import { Sidebar } from 'components/sidebar';
+import { SearchBar } from 'components/search-bar';
 
 export const Content = () => {
   return (
     <HStack width="full" flex={1} overflow="hidden">
       <Sidebar />
       <VStack px={12} pt={12} width="full" height="full" spacing={6} overflow="hidden">
-        {/* SearchBar */}
+        <SearchBar />
         {/* Genres Carousel */}
         <HStack width="full" alignItems="flex-start" overflow="hidden" flex={1}>
           {/* Top Picks */}
