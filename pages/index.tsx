297551d062f8b5ccbece44ed51cfb5f08099d727
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2023-02-16 03:05:14 +0100

    rendered the main Player component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { VStack } from '@chakra-ui/react';
+
+import { Player } from 'components/player';
 import { Content } from 'components/content';
 
 const IndexPage = () => (
   <VStack height="100vh" bg="black" width="full" overflow="hidden" spacing={0}>
     <Content />
-    {/* Player */}
+    <Player />
   </VStack>
 );
 
